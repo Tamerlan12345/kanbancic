bdae6ef1aa4c6df60b198c7edc9e97c6643ca5f8
Tamerlan12345 <centr-cool@mail.ru>
2025-10-07 17:23:02 +0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+    SUPABASE_URL: 'https://gqdwplbvxopanapxzlaw.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    GEMINI_API_KEY: 'AIzaSyCisFe9LE9ykOlc7JOn7NEJQDJ3LaMMFqI'
+};
