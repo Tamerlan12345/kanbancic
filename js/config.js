16238da8e410e189f11d0daaacdfc7ba70a41121
Tamerlan12345 <centr-cool@mail.ru>
2025-10-08 08:48:59 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // You can find your key in your Supabase project settings.
 export const APP_CONFIG = {
     SUPABASE_URL: 'https://gqdwplbvxopanapxzlaw.supabase.co',
-    SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY', // <-- REPLACE THIS WITH YOUR REAL KEY
-    GEMINI_API_KEY: 'YOUR_GEMINI_API_KEY' // <-- REPLACE THIS IF YOU USE AI FEATURES
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZHdwbGJ2eG9wYW5hcHh6bGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4MTk0MzYsImV4cCI6MjA3NTM5NTQzNn0.nk5jkXjN3gB1baycirRTU2nZelMklNf8CAWPkLamMLc', 
+    GEMINI_API_KEY: 'AIzaSyCisFe9LE9ykOlc7JOn7NEJQDJ3LaMMFqI' 
 };
